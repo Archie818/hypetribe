4ddde6763e9c29c46eed779e1c4c7d2b7f023c67
Archie <archie.yang@dynamocover.com>
2024-03-15 20:01:14 +0000

    changed page

---
+++ components/Footer.js
@@ -0,0 +1,12 @@
+const Footer = () => {
+	return (
+	  <footer className="bg-gray-800 text-white p-4">
+		<div className="container mx-auto text-center">
+		  © {new Date().getFullYear()} Hypetribe
+		</div>
+	  </footer>
+	);
+  };
+  
+  export default Footer;
+  
