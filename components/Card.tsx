90773a838adc1ece36818fb1b83325a37a46a4e1
Archie <archie.yang@dynamocover.com>
2024-03-16 11:43:42 +0000

    change type

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 interface CardProps {
   username: string;
   image: string;
@@ -8,7 +10,7 @@ interface CardProps {
 const Card: React.FC<CardProps> = ({ username, image, likes, time }) => {
   return (
     <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white">
-      <img className="w-full" src={image} alt={`Content by ${username}`} />
+      <Image className="w-full" src={image} alt={`Content by ${username}`} />
       <div className="px-6 py-4">
         <div className="font-bold text-xl mb-2">{username}</div>
         <p className="text-gray-700 text-base">
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import WorldIDWidget from "../components/WorldIDWidget";
+import Link from "next/link";
 
 interface HeaderProps {
   showWidget: boolean;
@@ -10,15 +11,15 @@ const Header: React.FC<HeaderProps> = ({ showWidget }) => {
     <header className="flex justify-between items-center py-4 text-white bg-opacity-90 bg-blue-500">
       <nav className="container mx-auto px-4 flex justify-between items-center">
         <div className="flex items-center space-x-4">
-          <a href="/" className="text-xl font-bold">
+          <Link href="/" className="text-xl font-bold">
             Hypetribe
-          </a>
-          <a href="/latest" className="hover:underline">
+          </Link>
+          <Link href="/latest" className="hover:underline">
             Latest
-          </a>
-          <a href="/explore" className="hover:underline">
+          </Link>
+          <Link href="/explore" className="hover:underline">
             Explore
-          </a>
+          </Link>
         </div>
         <div className="flex items-center space-x-4">
           <button className="py-2 px-4 bg-blue-500 hover:bg-blue-700 rounded">
